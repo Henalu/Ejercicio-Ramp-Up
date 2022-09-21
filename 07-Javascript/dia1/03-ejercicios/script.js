d2526dacfd165ec0ae0d34e8a23d75271548fad4
Henalu <henaludebarros@hotmail.com>
2022-09-21 21:24:23 +0200

    ejercicios-funciones

--- 07-Javascript/dia1/03-ejercicios/script.js
+++ 07-Javascript/dia1/03-ejercicios/script.js
@@ -7,90 +7,90 @@ function ejercicio1() {
     var numero3 = 0;
     var contador = 3;
 
-     do{
+    do {
         var numero1 = parseInt(prompt("Introduce el primer número", 0));
         var numero2 = parseInt(prompt("Introduce el segundo número", 0));
         var numero3 = parseInt(prompt("Introduce el tercer número", 0));
 
-        if(isNaN(numero1) || isNaN(numero2) || isNaN(numero3)){
+        if (isNaN(numero1) || isNaN(numero2) || isNaN(numero3)) {
             contador--;
             alert('Por favor, introduce solamente número, los caracteres no son válidos. Le quedan: ' + contador + ' intentos');
         }
-    }while ((isNaN(numero1) || isNaN(numero2) || isNaN(numero3)) && contador>0);
+    } while ((isNaN(numero1) || isNaN(numero2) || isNaN(numero3)) && contador > 0);
 
-    if(numero1==numero2 && numero1==numero3){
+    if (numero1 == numero2 && numero1 == numero3) {
         alert('Los tres numeros son iguales ');
-        }else if(numero1<numero2 && numero1<numero3){
-            alert('El menor de los numeros es: ' + numero1);
-        }else if(numero2<numero1 && numero2<numero3){
-            alert('El menor de los numeros es: ' + numero2);
-        }else if(numero3<numero1 && numero3<numero2){
-            alert('El menor de los numeros es: ' + numero3);
-        }
+    } else if (numero1 < numero2 && numero1 < numero3) {
+        alert('El menor de los numeros es: ' + numero1);
+    } else if (numero2 < numero1 && numero2 < numero3) {
+        alert('El menor de los numeros es: ' + numero2);
+    } else if (numero3 < numero1 && numero3 < numero2) {
+        alert('El menor de los numeros es: ' + numero3);
+    }
 }
 
-    //Ejercicio 2: Solicita al usuario una frase y una letra y muestra la cantidad de veces que aparece la letra en la frase.
-    //las palabras son arrays de cadenas de caracteres, asi que los podemos recorrer)
-    function ejercicio2() {
-        alert("Escribe una frase de al menos tres palabras diferentes y una letra");
+//Ejercicio 2: Solicita al usuario una frase y una letra y muestra la cantidad de veces que aparece la letra en la frase.
+//las palabras son arrays de cadenas de caracteres, asi que los podemos recorrer)
+function ejercicio2() {
+    alert("Escribe una frase de al menos tres palabras diferentes y una letra");
 
-        var frase = prompt("Escribe aquí tu frase"); // frase = lo que escriba el usuario
-        var letra = prompt("Escribe aquí tu letra"); // letra = letra que escoja el usuario
-        var cont = 0;
+    var frase = prompt("Escribe aquí tu frase"); // frase = lo que escriba el usuario
+    var letra = prompt("Escribe aquí tu letra"); // letra = letra que escoja el usuario
+    var cont = 0;
 
-        for (let i = 0; i < frase.length; i++) {
-            if (frase[i] == letra) {
-                cont++;
-            }
+    for (let i = 0; i < frase.length; i++) {
+        if (frase[i] == letra) {
+            cont++;
         }
-        alert("La letra " + letra + " aparece " + cont + " veces");
     }
+    alert("La letra " + letra + " aparece " + cont + " veces");
+}
 
-    //Ejercicio 3: Suma o resta (según elija el usuario) dos números reales => (se resuelve bien con un switch)
-    function ejercicio3() {
-        alert("Haz una suma o una resta")
+//Ejercicio 3: Suma o resta (según elija el usuario) dos números reales => (se resuelve bien con un switch)
+function ejercicio3() {
+    alert("Haz una suma o una resta")
 
-        var numero1 = parseFloat(prompt("Escribe el primer numero"));
+    var numero1 = parseFloat(prompt("Escribe el primer numero"));
 
-        var numero2 = parseFloat(prompt("Escribe aqui el segundo numero"));
+    var numero2 = parseFloat(prompt("Escribe aqui el segundo numero"));
 
-        do{ 
-            var decision = prompt("elija sumar o restar: + o -")
-            if(decision != '+' && decision != '-' && decision != 'restar' && decision != 'sumar'){
-                alert ('elija bien su opcion')
-            }
-        }while(decision != '+' && decision != '-' && decision != 'restar' && decision != 'sumar');
-    }
+    do {
+        var decision = prompt("elija sumar o restar: + o -")
+        if (decision != '+' && decision != '-' && decision != 'restar' && decision != 'sumar') {
+            alert('elija bien su opcion')
+        }
+    } while (decision != '+' && decision != '-' && decision != 'restar' && decision != 'sumar');
+}
 
-    //Ejercicio 4 (hay que hacerlo con un bucle Do While...)
-    //Almacena en dos variables datos de validación (usuario y contraseña) correctos y permite que el usuario valide. (dispone de 3 intentos)
-    function ejercicio4() {
+//Ejercicio 4 (hay que hacerlo con un bucle Do While...)
+//Almacena en dos variables datos de validación (usuario y contraseña) correctos y permite que el usuario valide. (dispone de 3 intentos)
+function ejercicio4() {
 
-        var usuario_fuengirola = 'fuengirola';
-        var contraseña_fuengirola = 'fuengirola2022';
-        var contador = 3;
-        var validacion = '';
+    var usuario_fuengirola = 'fuengirola';
+    var contraseña_fuengirola = 'fuengirola2022';
+    var contador = 3;
+    var validacion = '';
 
-        do{
+    do {
         var usuario = prompt("Escriba su nombre de Usuario");
         var contraseña = prompt("Escriba su contraseña");
-        if((usuario!=usuario_fuengirola || contraseña!=contraseña_fuengirola) && contador>0){
+        if ((usuario != usuario_fuengirola || contraseña != contraseña_fuengirola) && contador > 0) {
             contador--;
             alert('Su usuario o contraseña son incorrectos, pruebe nuevamente. Le quedan ' + contador + ' intentos');
-        }else{
+        } else {
             validacion = confirm('¿El usuario ' + usuario + ' y la contraseña ' + contraseña + ' son correctos?');
-            alert ('Se ha iniciado sesion correctamente')
+            alert('Se ha iniciado sesion correctamente')
         }
-        }while((usuario!=usuario_fuengirola || contraseña!=contraseña_fuengirola || validacion==false) && contador>0);
-    }
+    } while ((usuario != usuario_fuengirola || contraseña != contraseña_fuengirola || validacion == false) && contador > 0);
+}
 
-    //Ejercicio 5 (hacer un switch)
-    function ejercicio5() {
-        var letra = ' ';
-        var imprime = ' ';
-        do{ 
-            var letra = prompt("Escribe una letra");
-            switch (letra) {
+//Ejercicio 5 (hacer un switch)
+function ejercicio5() {
+    var letra = ' ';
+    var imprime = ' ';
+    do {
+        var letra = prompt("Escribe una letra");
+        switch (letra) {
             case 'a':
                 imprime = 7
                 break;
@@ -103,23 +103,53 @@ function ejercicio1() {
             default:
                 imprime = 'te has equivocado de letra'
                 break;
-            }
-            alert(imprime);
-        }while(imprime=='te has equivocado de letra')
-    }
+        }
+        alert(imprime);
+    } while (imprime == 'te has equivocado de letra')
+}
 
-    //Ejercicio 6 - La burbuja
-    function ejercicio6(){
-        var lista = ["burro", "caballo", "leon", "jirafa", "gorila", "aguila", "tigre"];
-        //decimos lista.lenght -1 porque el bucle va comparando elementos de dos en dos, entonces, en una lista de 7 elementos tienes 6 comparaciones.
-        for(let i=0; i < lista.length - 1 ; i++){ 
-            for(let j=0; j < lista.length - 1; j++){
-                if(lista[j] > lista[j+1]){
-                    [lista[j], lista[j+1]] = [lista[j+1], lista[j]];
-                }
+//Ejercicio 6 - La burbuja
+function ejercicio6() {
+    var lista = ["caballo", "burro", "leon", "jirafa", "gorila", "aguila", "tigre"];
+    //decimos lista.lenght -1 porque el bucle va comparando elementos de dos en dos, entonces, en una lista de 7 elementos tienes 6 comparaciones.
+    for (let i = 0; i < lista.length - 1; i++) {
+        for (let j = 0; j < lista.length - i- 1; j++) {
+            if (lista[j] > lista[j + 1]) {
+                [lista[j], lista[j + 1]] = [lista[j + 1], lista[j]];
             }
         }
+    }
 
     alert(lista);
     console.log(lista)
 }
+
+function prueba() {
+    var lista2 = [34, 245, 134, 2536, 3456, 1, 3, 0, 23, 54, 765, 90];
+
+    for (let i = 0; i < lista2.length - 1; i++) { //importante fijarse bien en la condicion para no iniciar un bucle infinito
+        for (let j = 0; j < lista2.length - i - 1; j++) {//le restamos a lista.lengt la i y 1 porque asumimos que el ultimo elemento ya esta ordenado y no hace falta repetir la comparacion
+            if (lista2[j] > lista2[j + 1]) {
+                [lista2[j], lista2[j + 1]] = [lista2[j + 1], lista2[j]];
+            }
+        }
+    }
+    console.log(lista2);
+}
+
+function correcion() { //las anteriores estan bien pero no sirven fuera de javascript
+    var lista3 = [34, 245, 134, 2536, 3456, 1, 3, 0, 23, 54, 765, 90];
+
+    for (let i = 0; i < lista3.length - 1; i++) { //importante fijarse bien en la condicion para no iniciar un bucle infinito
+        for (let j = 0; j < lista3.length - i - 1; j++) {//le restamos a lista.lengt la i y 1 porque asumimos que el ultimo elemento ya esta ordenado y no hace falta repetir la comparacion
+            if (lista3[j] > lista3[j + 1]) {
+                let aux = lista3[j];
+                lista3[j] = lista3[j+1];
+                lista3[j+1] = aux;
+            }
+        }
+    }
+    console.log(lista3);
+}
+
+
---
+++ 07-Javascript/dia2/04-ejercicios/script.js
@@ -0,0 +1,338 @@
+// 0. Una función que devuelva el número de numeros que hay hasta un entero solicitado al usuario //// Este me lo invente sin querer
+function ejercicio0() {
+    var numero_entero = 0;
+    var contador = 3;
+    do {
+        var numero_entero = parseInt(prompt('Introduce un numero entero'));
+        if (isNaN(numero_entero)) {
+            contador--;
+            alert('Por favor, introduzca unicamente números, los caracteres no son válidos, pruebe otra vez. Le quedan ' + contador + ' intentos');
+        } else if (numero_entero <= 0) {
+            contador--;
+            alert('Por favor, introduzca unicamente números mayores que cero, pruebe otra vez. Le quedan ' + contador + ' intentos');
+        }
+    } while ((isNaN(numero_entero) && contador > 0) || (contador > 0 && numero_entero <= 0));
+
+    // var numero_entero = toString(numero_entero); Sirve para transformar a string
+
+    for (var i = 1; i < numero_entero; i++) {
+        var cifras = [i];
+        cifras.length = i;
+    }
+    console.log(cifras);
+    alert('Entre 0 y el numero ' + numero_entero + ' hay ' + cifras.length + ' numeros');
+}
+
+// 1. Una función que devuelva el número de cifras de un entero solicitado al usuario //// No lo resolvi yo, pero me vale
+function ejercicio1() {
+    var numero_entero = 0;
+    var contador = 3;
+    do {
+        var numero_entero = parseInt(prompt('Introduce un numero entero'));
+        if (isNaN(numero_entero)) {
+            contador--;
+            alert('Por favor, introduzca unicamente números, los caracteres no son válidos, pruebe otra vez. Le quedan ' + contador + ' intentos');
+        } else if (numero_entero < 0) {
+            contador--;
+            alert('Por favor, introduzca unicamente números mayores que cero, pruebe otra vez. Le quedan ' + contador + ' intentos');
+        }
+    } while ((isNaN(numero_entero) && contador > 0) || (contador > 0 && numero_entero < 0));
+
+    console.log(numero_entero);
+    console.log(typeof (numero_entero));
+    var contador = 0;
+
+    do {
+        numero_entero = Math.floor(numero_entero / 10); //El Math.floor hace que se redondee la operacion
+        contador++;
+    } while (numero_entero != 0);
+
+    alert(contador);
+
+    /*
+    203/10 = 20     resto=3
+    20 / 10 = 2     resto=0
+    2 / 10 = 0      resto=2
+    */
+}
+
+// 2. Una función que muestre al usuario una secuencia de * (se debe construir la cadena de uno en uno), la cantidad de * será solicitada al usuario //// HECHO CON SUDOR Y LAGRIMAS
+var asteriscos = [''];
+var contadorAsteriscos = 0;
+
+function cantidadAsteriscos() {
+    do {
+        var decisionAsteriscos = parseInt(prompt('Introduzca con un número el tamaño de la secuencia de asteriscos que desea ver: '));
+        if (isNaN(decisionAsteriscos)) {
+            alert('Introduzca un numero, otro tipo de caracteres no son validos');
+        } else {
+            contadorAsteriscos = decisionAsteriscos;
+        }
+    } while (isNaN(decisionAsteriscos));
+
+    return contadorAsteriscos;
+}
+
+function ejercicio2() {
+    cantidadAsteriscos();
+    for (var i = 0; i < contadorAsteriscos; i++) {
+        asteriscos[i] = ['*'];
+        alert(asteriscos);
+    }
+    asteriscos = [''];
+}
+
+// 3. Una función que permita mostrar la secuencia (se debe construir la cadena de uno en uno): *+_*+_*+_*+_  //// HECHO CON SUDOR Y LAGRIMAS
+var simbolos = [''];
+var contadorSimbolos = 0;
+
+function cantidadSimbolos() {
+    do {
+        var decisionSimbolos = parseInt(prompt('Indique con un numero entero cuan grande quiere que sea la cadena de simbolos'));
+        if (isNaN(decisionSimbolos)) {
+            alert('Introduzca numeros validos, otro tipo de caracteres no lo son')
+        } else {
+            contadorSimbolos = decisionSimbolos
+        }
+    } while (isNaN(decisionSimbolos)) {
+
+    }
+    return contadorSimbolos;
+}
+
+function ejercicio3() {
+    cantidadSimbolos();
+    for (i = 0; i < contadorSimbolos; i++) {
+        simbolos[i] = ['*+_'];
+        alert(simbolos);
+    }
+    contadorSimbolos = ['']; // Para que el contador se reinicie y podamos repetir desde cero cada vez que apretamos el boton ;)
+}
+//4. Una función que permita mostrar un triángulo como el siguiente:
+/*
+
+*
+**
+***
+****
+*****     
+
+*/
+function prueba() {
+    var confirmacion = confirm('¿Quiere ver un triangulo de asteriscos?')
+    if (confirmacion == true) {
+        alert(
+            '*' + '\n'
+            + '**' + '\n'
+            + '***' + '\n'
+            + '****' + '\n'
+            + '*****');
+    } else {
+        alert('Hasta la proxima')
+    }
+}
+
+var elementosTriangulo = [''];
+var contadorTriangulo = 0;
+
+function elegirElementos() {
+    do {
+        var decisionElementos = parseInt(prompt('Elija el numero de filas (altura) que quiere que tenga la piramide'))
+        if (isNaN(decisionElementos)) {
+            alert('Solamente los numeros son validos, pruebe otra vez')
+        } else {
+            contadorTriangulo = decisionElementos;
+        }
+    } while (isNaN(decisionElementos));
+    return contadorTriangulo;
+}
+
+function ejercicio4() {
+    elegirElementos();
+    for (i = 0; i < contadorTriangulo; i++) {
+        elementosTriangulo[i] = ['\n *'];
+        alert(elementosTriangulo);
+    }
+    elementosTriangulo = ['*'];
+}
+
+// 5. Una función que devuelva la diferencia en días entre dos fechas del mismo año (sólo tenemos en cuenta dia y mes)
+var año = 360;
+var mesesDelAño = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']; //me gustaria añadir que si el usuario introduce mal el dia del mes no lo acepte y vuelva a pedirlo, pero no se como hacerlo con un array. 
+var diaDelAño = 0;
+var dia = 0;
+var mes = [''];
+
+var diaDelAño2 = 0;
+var dia2 = 0;
+var mes2 = [''];
+
+diferencia = 0;
+
+function elegirDia() {
+    do {
+        var seleccionarDia = parseInt(prompt('Introduce el día del mes:'));
+        if (isNaN(seleccionarDia)) {
+            alert('por favor introduce correctamente el día del mes');
+        }
+    } while (isNaN(seleccionarDia));
+
+    dia = seleccionarDia;
+    return dia;
+}
+
+function elegirMes() {
+    do {
+        var seleccionarMes = prompt('Introduce el mes:');
+        if (!isNaN(seleccionarMes)) {
+            alert('por favor, introduzca el mes correctamente')
+        }
+    } while (!isNaN(seleccionarMes));
+
+    mes = seleccionarMes;
+    return mes;
+}
+
+function contadorDias() {
+    switch (mes) {
+        case 'enero':
+            diaDelAño = dia;
+            break;
+        case 'febrero':
+            diaDelAño = (dia + 30);
+            break;
+        case 'marzo':
+            diaDelAño = (dia + 60);
+            break;
+        case 'abril':
+            diaDelAño = (dia + 90);
+            break;
+        case 'mayo':
+            diaDelAño = (dia + 120);
+            break;
+        case 'junio':
+            diaDelAño = (dia + 150);
+            break;
+        case 'julio':
+            diaDelAño = (dia + 180);
+            break;
+        case 'agosto':
+            diaDelAño = (dia + 210);
+            break;
+        case 'septiembre':
+            diaDelAño = (dia + 240);
+            break;
+        case 'octubre':
+            diaDelAño = (dia + 270);
+            break;
+        case 'noviembre':
+            diaDelAño = (dia + 300);
+            break;
+        case 'diciembre':
+            diaDelAño = (dia + 330);
+            break;
+    }
+
+    return diaDelAño;
+}
+
+function elegirDia2() {
+    do {
+        var seleccionarDia = parseInt(prompt('Introduce el día del mes:'));
+        if (isNaN(seleccionarDia)) {
+            alert('por favor introduce correctamente el día del mes');
+        }
+    } while (isNaN(seleccionarDia));
+
+    dia2 = seleccionarDia;
+    return dia2;
+}
+
+function elegirMes2() {
+    do {
+        var seleccionarMes = prompt('Introduce el mes:');
+        if (!isNaN(seleccionarMes)) {
+            alert('por favor, introduzca el mes correctamente')
+        }
+    } while (!isNaN(seleccionarMes));
+
+    mes2 = seleccionarMes;
+    return mes2;
+}
+
+function contadorDias2() {
+    switch (mes2) {
+        case 'enero':
+            diaDelAño2 = dia;
+            break;
+        case 'febrero':
+            diaDelAño2 = (dia + 30);
+            break;
+        case 'marzo':
+            diaDelAño2 = (dia + 60);
+            break;
+        case 'abril':
+            diaDelAño2 = (dia + 90);
+            break;
+        case 'mayo':
+            diaDelAño2 = (dia + 120);
+            break;
+        case 'junio':
+            diaDelAño2 = (dia + 150);
+            break;
+        case 'julio':
+            diaDelAño2 = (dia + 180);
+            break;
+        case 'agosto':
+            diaDelAño2 = (dia + 210);
+            break;
+        case 'septiembre':
+            diaDelAño2 = (dia + 240);
+            break;
+        case 'octubre':
+            diaDelAño2 = (dia + 270);
+            break;
+        case 'noviembre':
+            diaDelAño2 = (dia + 300);
+            break;
+        case 'diciembre':
+            diaDelAño2 = (dia + 330);
+            break;
+    }
+
+    return diaDelAño2;
+}
+
+function fecha1() { //Engloba tres funciones dentro de ella
+    elegirDia();
+    elegirMes();
+    contadorDias();
+    alert('La primera fecha elegida se corresponde al dia '+ dia + ' del mes de ' + mes + ' que equivale al dia del año: ' + diaDelAño)
+}
+
+function fecha2() { //Engloba tres funciones dentro de ella
+    elegirDia2();
+    elegirMes2();
+    contadorDias2();
+    alert('La segunda fecha elegida se corresponde al dia '+ dia2 + ' del mes de ' + mes2 + ' que equivale al dia del año: ' + diaDelAño2)
+}
+
+function ejercicio5() { //Funcion Final que engloba las anteriores unas dentro de otras.
+    fecha1();
+    fecha2();
+
+    if(diaDelAño>diaDelAño2){
+        var resultado = (diaDelAño - diaDelAño2);
+        diferencia=resultado;
+        alert('Hay una diferencia de ' + diferencia + ' dias entre la primera fecha elegida y la segunda');
+    }else if (diaDelAño2>diaDelAño){
+        var resultado = (diaDelAño2 - diaDelAño);
+        diferencia=resultado;
+        alert('Hay una diferencia de ' + diferencia + ' dias entre la segunda fecha elegida y la primera');
+    }else{
+        alert ('ambas fechas se corresponden al mismo día del año');
+        diferencia = 0;
+    }
+
+    
+}
